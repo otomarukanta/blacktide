274a775cd0cd8a93bdc530a4952369b324364c3a
otomarukanta <kanta208@gmail.com>
2016-07-31 16:26:07 +0900

    Add Item

--- blacktide/items.py
+++ blacktide/items.py
@@ -8,7 +8,30 @@
 import scrapy
 
 
-class BlacktideItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+class RaceResultItem(scrapy.Item):
+    race_id = scrapy.Field()
+    race_no = scrapy.Field()
+    info = scrapy.Field()
+    payoff = scrapy.Field()
+    result = scrapy.Field()
+    schedule = scrapy.Field()
+    race_name = scrapy.Field()
+    weather = scrapy.Field()
+    distance = scrapy.Field()
+    qualification = scrapy.Field()
+    condition = scrapy.Field()
+    prize = scrapy.Field()
+    ground_condition = scrapy.Field()
+
+
+class HorseItem(scrapy.Item):
+    horse_id = scrapy.Field()
+    sex_age = scrapy.Field()
+    horse_name = scrapy.Field()
+    birthday = scrapy.Field()
+    color = scrapy.Field()
+    trainer = scrapy.Field()
+    owner = scrapy.Field()
+    producer = scrapy.Field()
+    birthplace = scrapy.Field()
+    blood = scrapy.Field()
--- blacktide/parsers/horse_parser.py
+++ blacktide/parsers/horse_parser.py
@@ -9,5 +9,15 @@ class HorseParser():
         item = HorseItem()
 
         item['horse_id'] = self.id_regex.match(res.url).group(1)
+        item['sex_age'] = res.xpath(
+                '//div[@id="dirTitName"]/p/text()').extract()
+        item['horse_name'] = res.xpath(
+                '//div[@id="dirTitName"]/h1/text()').extract_first()
+        item['birthday'], item['color'], item['trainer'], \
+            item['owner'], item['producer'], item['birthplace'] = res.xpath(
+                '//div[@id="dirTitName"]/ul/li/text()').extract()
+
+        item['blood'] = res.xpath(
+            '//table[@id="dirUmaBlood"]/*/td/text()').extract()
 
         return item
