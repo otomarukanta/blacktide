154078b35f173003c611f5b13c3db6db9205ff15
otomarukanta <kanta208@gmail.com>
2016-10-30 17:24:39 +0900

    Add jockey and horse parser in tminazuma

--- blacktide/spiders/tminazuma.py
+++ blacktide/spiders/tminazuma.py
@@ -1,20 +1,30 @@
 # -*- coding: utf-8 -*-
+import re
 from scrapy.linkextractors import LinkExtractor
 from scrapy.spiders import CrawlSpider, Rule
 
 from blacktide.parsers import RaceResultParser
+from blacktide.parsers import HorseParser, JockeyParser
 
 
 class TminazumaSpider(CrawlSpider):
     name = 'tminazuma'
     allowed_domains = ['keiba.yahoo.co.jp']
     _race_result_parser = RaceResultParser()
+    regex_race_result = re.compile(r'/race/result/')
 
     rules = (
         Rule(LinkExtractor(allow=r'/race/list/')),
         Rule(LinkExtractor(allow=r'/race/result/'),
-             follow=False,
+             follow=True,
+             process_links='exclude_race_result',
              callback='parse_result'),
+        Rule(LinkExtractor(allow=r'/directory/horse/\d+/\Z'),
+             follow=False,
+             callback=HorseParser.parse),
+        Rule(LinkExtractor(allow=r'/directory/jocky/'),
+             follow=False,
+             callback=JockeyParser.parse),
     )
 
     def __init__(self, *args, **kargs):
@@ -26,3 +36,6 @@ class TminazumaSpider(CrawlSpider):
         parsed = self._race_result_parser.parse(response)
         self.logger.debug("done")
         return parsed
+
+    def exclude_race_result(self, links):
+        return filter(lambda x: not self.regex_race_result.search(x.url), links)
